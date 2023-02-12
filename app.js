fee6dbb84e44470504459c2e3e65b547d81714e9
Mikaszz <125219320+Mikaszz@users.noreply.github.com>
2023-02-12 22:43:46 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+let display = document.getElementById('tekstas');
+let buttons = Array.from(document.getElementsByClassName('btn-operator'));
+
+buttons.map( button => {
+    button.addEventListener('click',(e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '←':
+                    if(display.innerText){
+                        display.innerText = display.innerText.slice(0,-1);
+                    }
+                    break;
+                case '=':
+                        try{
+                            display.innerText = eval(display.innerText);
+                        } catch {
+                            display.innerText = 'klaida!';
+                        }
+
+                        break;
+                        default:
+                            display.innerText += e.target.innerText; 
+                                         
+        }
+    });
+});
+
+
+
